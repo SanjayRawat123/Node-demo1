d1c891e94a0f1322c2d76e3884c072a511eb5198
Sanjay <rawatsanjay2799@gmail.com>
2023-08-02 09:07:48 +0530

    chnages made in node.js created url

--- node.js
+++ node.js
@@ -3,8 +3,16 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.end("hello from server")
+  console.log(req.url);
+const pathName = req.url;
+if ( pathName== "/"||pathName == "overview"){
+    res.end("this is overview");
+
+}else if(pathName == "/product"){
+  
+res.end("this is product view");
+}
+
 })
 
 server.listen(8000, '127.0.0.1', () => {

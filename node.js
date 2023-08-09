742aef82255ef11dd886a6a761875d80ac38722e
Sanjay <rawatsanjay2799@gmail.com>
2023-08-09 09:38:37 +0530

    chnages made in server

--- node.js
+++ node.js
@@ -1,6 +1,12 @@
+const { error } = require('console');
 const fs = require('fs');
 const http = require('http');
+const { json } = require('stream/consumers');
 const url = require('url');
+//server
+const data =fs.readFileSync(`${__dirname}/data/data.json` , 'utf-8' )
+const dataObj = JSON.parse(data);
+   
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
@@ -11,7 +17,17 @@ if ( pathName== "/"||pathName == "overview"){
 }else if(pathName == "/product"){
   
 res.end("this is product view");
-}
+}else if(pathName==="/api"){
+  res.writeHead(200, {'Content-type': 'application/json'});
+  res.end(data);
+
+}else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 
 })
 

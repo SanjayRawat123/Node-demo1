6e546f4c330c26d6a00c64bde463dd395528f172
Sanjay <rawatsanjay2799@gmail.com>
2023-09-06 08:55:36 +0530

    added the navigation and modify the code

--- node.js
+++ node.js
@@ -4,6 +4,21 @@ const http = require('http');
 const { json } = require('stream/consumers');
 const url = require('url');
 //server
+
+const replaceTemplate = (temp , product)=>{
+  let output = temp.replace(/{%PRODUCTNAME%}/g , product.productName);
+  output = output.replace(/{%IMAGE%}/g , product.image );
+  output = output.replace(/{%PRICE%}/g , product.price );
+  output = output.replace(/{%FROM%}/g , product.from );
+  output = output.replace(/{%QUANTITY%}/g , product.quantity );
+  output = output.replace(/{%ID%}/g , product.id );
+  if(!product.organic)output = output.replace(/{%NOT_ORGANIC%}/g ,'not-organic' );
+  output = output.replace(/{%DESCRIPTION%}/g , product.description );
+  return output;
+     
+     
+     
+}
 const data =fs.readFileSync(`${__dirname}/data/data.json` , 'utf-8' );
 const tempOverview =fs.readFileSync(`${__dirname}/template/template-overview.html` , 'utf-8' );
 
@@ -13,16 +28,24 @@ const dataObj = JSON.parse(data);
   
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-const pathName = req.url;
-if ( pathName== "/"||pathName == "overview"){
-  res.writeHead(200 , {'Content-type':'text/html'});
-    res.end(tempOverview);
 
-}else if(pathName == "/product"){
-  
-res.end("this is product view");
-}else if(pathName==="/api"){
+  console.log(req.url);
+  const {query , pathname} = url.parse(req.url , true);
+// const pathName = req.url;
+if ( pathname== "/"||pathname == "overview"){
+  res.writeHead(200 , {'Content-type':'text/html'});
+  const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+  const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+  // console.log(cardsHtml);
+  console.log(req.url);
+    res.end(output);
+
+}else if(pathname == "/product"){
+  console.log(query);
+  const product = dataObj[query.id];
+  const output = replaceTemplate(tempProduct , product);
+res.end(output);
+}else if(pathname==="/api"){
   res.writeHead(200, {'Content-type': 'application/json'});
   res.end(data);
 
